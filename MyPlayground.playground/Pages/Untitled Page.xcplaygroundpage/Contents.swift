ea2116ac5fc1adcc162ead49b76df44fca8d3213
jieun <jieun_0330@naver.com>
2023-09-17 22:53:56 +0900

    CH01_22.열거형

--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -818,26 +818,78 @@ import UIKit
  }
  */
 
-extension Int {
-    var isEven: Bool {
-        return self % 2 == 0
-    }
-    
-    var isOdd: Bool {
-        return self % 2 == 1
-    }
+//extension Int {
+//    var isEven: Bool {
+//        return self % 2 == 0
+//    }
+//
+//    var isOdd: Bool {
+//        return self % 2 == 1
+//    }
+//}
+//
+//var number = 3
+//number.isOdd
+//number.isEven
+//
+//
+//extension String {
+//    func convertToInt() -> Int? {
+//        return Int(self)
+//    }
+//}
+//
+//var string = "0"
+//string.convertToInt()
+
+
+
+
+
+
+
+
+//CH01_22.열거형
+// 연관성이 있는 값을 모아 놓은 것을 말한다
+
+enum CompassPoint: String {
+//    case north, south, east, west
+    case north = "북"
+    case south = "남"
+    case east = "동"
+    case west = "서"
 }
 
-var number = 3
-number.isOdd
-number.isEven
+var direction = CompassPoint.east
+direction = .west
 
-
-extension String {
-    func convertToInt() -> Int? {
-        return Int(self)
-    }
+switch direction {
+case .north:
+    print(direction.rawValue)
+case .south:
+    print(direction.rawValue)
+case .east:
+    print(direction.rawValue)
+case .west:
+    print(direction.rawValue)
 }
 
-var string = "0"
-string.convertToInt()
+
+let direction2 = CompassPoint(rawValue: "남")
+
+
+
+enum PhoneError {
+    case unknown
+    case batteryLow(String)
+}
+
+let error = PhoneError.batteryLow("배터리가 곧 방전됩니다")
+
+
+switch error {
+case .batteryLow(let message):
+    print(message)
+case .unknown:
+    print("알 수 없는 에러입니다.")
+}
