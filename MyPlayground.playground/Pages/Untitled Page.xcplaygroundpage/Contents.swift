55f966885fe564bf191ac913e72fa25da18d1d37
jieun <jieun_0330@naver.com>
2023-09-17 22:31:39 +0900

    CH01_20.프로토콜

--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -728,3 +728,79 @@ func guardTest(value: Int?) {
 
 guardTest(value: 2)
 guardTest(value: nil)
+
+
+
+
+
+//CH01_20.프로토콜
+// 특정 역할을 하기 위한 메서드, 프로퍼티, 기타 요구사항 등의 청사진
+
+/* 복붙
+ protocol 이름 {
+ 
+ }
+ */
+
+protocol SomeProtocol {
+    
+}
+
+protocol SomeProtocol2 {
+    
+}
+
+struct SomeStructure: SomeProtocol, SomeProtocol2 {
+    
+}
+
+/*
+ class someClass: SomeSuperclass, FisrtProtocol, AnotherProtocol {
+ 
+ }
+ */
+
+
+protocol FirstProtocol {
+    // 읽기, 쓰기
+    var name: Int { get set }
+    // 읽기 전용
+    var age: Int { get }
+}
+
+// type property를 요구하려면 static 키워드를 써야한다
+protocol AnotherProtocol {
+    static var someTypeProperty: Int { get set }
+}
+
+protocol FullyNames {
+    var fullName: String { get set }
+    func printFullName()
+}
+
+struct Person: FullyNames {
+    var fullName: String
+    
+    func printFullName() {
+        print(fullName)
+    }
+}
+
+protocol SomeProtocol3 {
+    func someTypeMethod()
+}
+
+protocol SomeProtocol4 {
+    init(someParameter: Int)
+}
+
+protocol SomeProtocol5 {
+    init()
+}
+
+//생성자 요구사항을 준수하려면 required 식별자가 필요하다
+class SomeClass: SomeProtocol5 {
+    required init() {
+        
+    }
+}
