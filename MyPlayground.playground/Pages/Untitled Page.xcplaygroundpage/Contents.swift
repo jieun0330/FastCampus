74b79743f453efeff11d17bac879bb7bc93738db
jieun <jieun_0330@naver.com>
2023-09-17 17:59:10 +0900

    CH01_16.클래스와 구조체의 차이

--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -505,3 +505,53 @@ SomeStructure.computedTypeProperty
 SomeStructure.storedTypeProperty
 SomeStructure.storedTypeProperty = "hello"
 SomeStructure.storedTypeProperty
+
+
+
+
+
+//CH01_16.클래스와 구조체의 차이
+/*
+ 클래스와 구조체의 공통점
+ - 값을 저장할 프로퍼티를 선언할 수 있다
+ - 함수적 기능을 하는 메서드를 선언할 수 있다
+ - 내부 값에 .을 사용하여 접근할 수 있다
+ 
+ 클래스
+ - 참조 타입
+ - 같은 클래스 인스턴스를 여러 개의 변수에 할당한 뒤 값을 변경 시키면 모든 변수에 영향을 줌
+ 
+ 구조체
+ - 값 타입
+ - 구조체 변수를 새로운 변수에 할당할 때마다 새로운 구조체가 할당된다.
+ - 즉 같은 구조체를 여러 개의 변수에 할당한 뒤 값을 변경시키더라도 다른 변수에 영향을 주지 않는다
+ */
+
+//참조 타입
+class SomeClass {
+    var count: Int = 0
+}
+
+struct SomeStruct {
+    var count: Int = 0
+}
+
+var class1 = SomeClass()
+var class2 = class1
+var class3 = class1
+
+class3.count = 2
+class1.count
+
+var struct1 = SomeStruct()
+var struct2 = struct1
+var struct3 = struct1
+
+struct2.count = 3
+struct3.count = 4
+
+struct1.count
+struct2.count
+struct3.count
+
+//값 타입이라 값을 변경하더라도 다른데에 영향을 주지 않는다
